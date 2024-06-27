5df4bfa0d3663c7c4bea3b3977ad72df81b8cc06
lee mwangi <lee.mwangi1@student.moringaschool.com>
2024-06-27 11:47:56 +0300

    commiting variables asingment

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 
 // Write your code here
+const num1 = 31;
+const num2 =2;
+const multiply = num1*num2
+console.log(multiply);
+const random = Math.floor(Math.random() * 100) + 1;
+
+console.log(random);
+// Define the variables
+const num3 = 9;
+const num4 = 5;
+
+// Create the mod variable
+const mod = num3 % num4;
+
+console.log(mod); // Should output 4
+// Define the set of numbers
+const numbers = [1, 5, 10, 15, 20];
+
+// Find the highest number in the set
+const max = Math.max(...numbers);
+
+console.log(max); // Should output 20
